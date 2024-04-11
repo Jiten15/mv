558fb93154a121ec5b7eee9d98107aefbdeb7383
jitendra chautharia <89199092+Jiten15@users.noreply.github.com>
2024-04-11 13:17:38 +0530

    Update script_breakdown.py

--- script_breakdown.py
+++ script_breakdown.py
@@ -257,10 +257,12 @@ uploaded_files = st.file_uploader("Upload multiple files", accept_multiple_files
 if uploaded_files:
     for uploaded_file in uploaded_files:
         st.write("Filename:", uploaded_file.name)
-
-if uploaded_file is not None:
 	output=script_breakdown(uploaded_file.name)
 	st.write(output)
+
+# if uploaded_file is not None:
+# 	output=script_breakdown(uploaded_file.name)
+# 	st.write(output)
 
 if st.button('download csv'):
 	output.to_csv("output.csv")
